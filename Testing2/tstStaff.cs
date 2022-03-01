7560736b38966ee5302eebb47f099d230647f773
P2594542 <P2594542@GWHXX652WIN006W>
2022-03-01 14:01:23 +0000

    begin simple test InstanceOk and StaffNameOK.

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff); //AStaff object should not be null.
+        }
+        [TestMethod]
+        public void StaffNameOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            string TestData = "Dorota Paczkowska";
+            AStaff.StaffName = TestData; //description assign to test data 
+            Assert.AreEqual(AStaff.StaffName, TestData); //AStaff object should not be null.
+        }
+
+       
+    }
+}
