ec8b25c1d21065e486751952582f575effcfd206
Emmanuel Ikharo <P2525695@my365.dmu.ac.uk>
2022-03-04 23:30:51 +0000

    Created the properties for supplier Table Emmanuel

--- ClassLibrary/clsSuppliers.cs
+++ ClassLibrary/clsSuppliers.cs
@@ -2,5 +2,12 @@
 {
     public class clsSuppliers
     {
+        public bool SupplierAvailable { get; set; }
+        public int SuppliersId { get; set; }
+        public long SuppliersCountectNo { get; set; }
+        public long SupplierContactNo { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierEmail { get; set; }
+        public string SupplierAddress { get; set; }
     }
 }
--- Testing5/tstSuppliers.cs
+++ Testing5/tstSuppliers.cs
@@ -16,5 +16,63 @@ namespace Testing5
             Assert.IsNotNull(AnSuppliers);
 
         }
+        [TestMethod]
+        public void SupplierAvailablePropertOK()
+        {
+            //create an instance of the class we want to create
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            //create some of the test data
+            Boolean TestData = true;
+            //assign the data to the property
+            AnSuppliers.SupplierAvailable = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnSuppliers.SupplierAvailable, TestData);
+        }
+        [TestMethod]
+        public void SupplierIdPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnSuppliers.SuppliersId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnSuppliers.SuppliersId, TestData);
+        }
+        [TestMethod]
+        public void SupplierContactNoPropertyOk()
+        {
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            Int64 TestData = 07099772331;
+            AnSuppliers.SupplierContactNo = TestData;
+            Assert.AreEqual(AnSuppliers.SupplierContactNo, TestData);
+
+        }
+        [TestMethod]
+        public void SupplierNameOK()
+        {
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            string TestData = "MIK Rescue";
+            AnSuppliers.SupplierName = TestData;
+            Assert.AreEqual(AnSuppliers.SupplierName, TestData);
+        }
+        [TestMethod]
+        public void SupplierEmailOK()
+        {
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            string TestData = "supplier@mail.com";
+            AnSuppliers.SupplierEmail = TestData;
+            Assert.AreEqual(AnSuppliers.SupplierEmail, TestData);
+
+        }
+        [TestMethod]
+        public void SupplierAddressOK()
+        {
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            string TestData = "22 madden street Leicester";
+            AnSuppliers.SupplierAddress = TestData;
+            Assert.AreEqual(AnSuppliers.SupplierAddress, TestData);
+        }
     }
 }
