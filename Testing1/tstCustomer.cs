ead0d34ba6526f4200b46b9d4722d463e56497a1
Angham Alkousa <P2594704@my365.dmu.ac.uk>
2022-03-09 01:37:48 +0000

    Added testing for address class

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstancOK()
+        {
+            //To create an instance of the class 
+            clsCustomer Customer = new clsCustomer();
+            //Test to see that it exists
+            Assert.IsNotNull(Customer);
+
+        }
+    }
+}
