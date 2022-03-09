de8b48418021ce47b073a4e1cb00fd0812f41667
Angham Alkousa <P2594704@my365.dmu.ac.uk>
2022-03-09 05:03:56 +0000

    Created the properties for the customer table design

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool IsStudent { get; set; }
+        public string CustomerName { get; set; }
+        public double StudnetDisscountPercentage { get; set; }
+        public DateTime CustomerDOB { get; set; }
+        public int CustomerID { get; set; }
     }
 }
--- Testing1/tstCustomer.cs
+++ Testing1/tstCustomer.cs
@@ -16,5 +16,57 @@ namespace Testing1
             Assert.IsNotNull(Customer);
 
         }
+        [TestMethod]
+        public void IsStudentPropertyOK()
+        {
+            //To create an instance of the class 
+            clsCustomer Customer = new clsCustomer();
+
+            Boolean TestData = true;
+
+            Customer.IsStudent = TestData;
+            Assert.AreEqual(Customer.IsStudent, TestData);
+
+        }
+        public void CustomerNamePropertyOK()
+        {
+            //To create an instance of the class 
+            clsCustomer Customer = new clsCustomer();
+            string TestData = "John Blogger";
+            Customer.CustomerName = TestData;
+            Assert.AreEqual(Customer.CustomerName, TestData);
+
+        }
+        public void StudnetDisscountPercentageOK()
+        {
+            //To create an instance of the class 
+            clsCustomer Customer = new clsCustomer();
+            double TestData = 1.5;
+            Customer.StudnetDisscountPercentage = TestData;
+            Assert.AreEqual(Customer.StudnetDisscountPercentage, TestData);
+
+        }
+        public void CustomerDOBOK()
+        {
+            //To create an instance of the class 
+            clsCustomer Customer = new clsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            Customer.CustomerDOB = TestData;
+            Assert.AreEqual(Customer.CustomerDOB, TestData);
+
+        }
+        public void CustomerIDOK()
+        {
+            //To create an instance of the class 
+            clsCustomer Customer = new clsCustomer();
+            Int32 TestData = 1;
+
+            Customer.CustomerID = TestData;
+            Assert.AreEqual(Customer.CustomerID, TestData);
+
+        }
+
+
+
     }
 }
