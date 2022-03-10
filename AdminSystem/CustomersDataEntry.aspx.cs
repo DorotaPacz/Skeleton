4d6a262d5316fb6934e6e72a04c2bd491608ee7b
Angham Alkousa <P2594704@my365.dmu.ac.uk>
2022-03-10 03:24:29 +0000

    Added presentation layer

--- AdminSystem/CustomersDataEntry.aspx.cs
+++ AdminSystem/CustomersDataEntry.aspx.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        clsCustomer Customer = new clsCustomer();
+
+        Customer.CustomerName = txtCustomerName.Text;
+      
+        Session["Customer"] = Customer;
+
+      //  Customer.CustomerDOB = txtCustomerDOB.Text;
+
+       // Session["Customer"] = Customer;
+
+        //Customer.CustomerID = txtCustomerID.Text;
+
+       //Session["Customer"] = Customer;
+
+        
+         //navigate to the viewer page
+        Response.Redirect("CustomersViewer.aspx");
+
+
+
+    }
+
+    protected void txtCustomerID_TextChanged(object sender, EventArgs e)
     {
 
     }
