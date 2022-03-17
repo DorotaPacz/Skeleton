197a6435430013c82a4beadbc57f5fef5b7def23
Igor Miholancan <P2518523@my365.dmu.ac.uk>
2022-03-17 16:28:59 +0000

    Added more tests.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Available { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int StockItemNo { get; set; }
+        public string StockItemDescription { get; set; }
+        public double Price { get; set; }
+        public int QuantityInStock { get; set; }
     }
 }
