7326ae47ffa71868787b5a6ddfc2c05e69ecc732
Dawid Markieton <P2545987@my365.dmu.ac.uk>
2022-03-18 11:37:24 +0000

    Added find method for orderline class, added testing methods and hardcoded tests data in orderline class, week 23 part 1

--- ClassLibrary/clsOrderLine.cs
+++ ClassLibrary/clsOrderLine.cs
@@ -1,9 +1,69 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrderLine
     {
-        public int StockItemNo { get; set; }
-        public int OrderID { get; set; }
-        public int Quantity { get; set; }
+        //private data member for the StockItemNo property
+        private Int32 privateStockItemNo;
+        //StockItemNo public property
+        public Int32 StockItemNo
+        {
+            get
+            {
+                //this line of the code sends data out of the property
+                return privateStockItemNo;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                privateStockItemNo = value;
+            }
+        }
+
+        //private data member for the OrderID property
+        private Int32 privateOrderID;
+        //OrderID public property
+        public Int32 OrderID
+        {
+            get
+            {
+                //this line of the code sends data out of the property
+                return privateOrderID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                privateOrderID = value;
+            }
+        }
+
+        //private data member for the OrderID property
+        private Int32 privateQuantity;
+        //OrderID public property
+        public Int32 Quantity
+        {
+            get
+            {
+                //this line of the code sends data out of the property
+                return privateQuantity;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                privateQuantity = value;
+            }
+        }
+
+
+        public bool Find(int stockItemNo)
+        {
+            //hardcoded data
+            privateStockItemNo = 1;
+            privateOrderID = 1;
+            privateQuantity = 1;
+            //always return true
+            return true;
+        }
     }
 }
