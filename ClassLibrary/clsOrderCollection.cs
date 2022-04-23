7692a3669c87c4682e3bcd519b77e146b195f528
Dawid Markieton <P2545987@my365.dmu.ac.uk>
2022-04-23 15:55:03 +0100

    added properties to OrderCollection and their tests

---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+        public List<clsOrder> OrderList { get; set; }
+        public int Count { get; set; }
+        public clsOrder ThisOrder { get; set; }
+    }
+}
---
+++ Testing4/tstOrderCollection.cs
@@ -0,0 +1,64 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderCollection aCollection = new clsOrderCollection();
+
+            Assert.IsNotNull(aCollection);
+        }
+
+        [TestMethod]
+        public void OrderListOK()
+        {
+            clsOrderCollection aCollection = new clsOrderCollection();
+
+            List<clsOrder> testList = new List<clsOrder>();
+
+            clsOrder anOrder = new clsOrder();
+            anOrder.ID = 15;
+            anOrder.Date = DateTime.Now.Date;
+            anOrder.IsFulfilled = false;
+            anOrder.TotalPrice = 19.99f;
+
+            testList.Add(anOrder);
+            aCollection.OrderList = testList;
+
+            Assert.AreEqual(aCollection.OrderList, testList);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            clsOrderCollection aCollection = new clsOrderCollection();
+            Int32 SomeCount = 0;
+            aCollection.Count = SomeCount;
+            Assert.AreEqual(aCollection.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisOrderPropertyOK()
+        {
+            clsOrderCollection aCollection = new clsOrderCollection();
+
+            clsOrder anOrder = new clsOrder();
+            anOrder.ID = 15;
+            anOrder.Date = DateTime.Now.Date;
+            anOrder.IsFulfilled = false;
+            anOrder.TotalPrice = 19.99f;
+
+            aCollection.ThisOrder = anOrder;
+            Assert.AreEqual(aCollection.ThisOrder, anOrder);
+
+        }
+
+    }
+}
