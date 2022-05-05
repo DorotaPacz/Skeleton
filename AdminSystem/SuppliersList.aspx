04f036020f76428970a40c56486b6b3ab7b1d5fe
Emmanuel Ikharo <P2525695@my365.dmu.ac.uk>
2022-05-05 03:43:42 +0100

    week 26

--- AdminSystem/SuppliersList.aspx
+++ AdminSystem/SuppliersList.aspx
@@ -9,6 +9,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:ListBox ID="IstSuppliersList" runat="server" Height="359px" Width="375px"></asp:ListBox>
         </div>
     </form>
 </body>
---
+++ ClassLibrary/clsSuppliersCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsSuppliersCollection
+    {
+        //private data member for the list
+        List<clsSuppliers> mSuppliersList = new List<clsSuppliers>();
+        public List<clsSuppliers> SuppliersList
+        {
+            get
+            {
+                return mSuppliersList;
+            }
+            set
+            {
+                mSuppliersList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mSuppliersList.Count;
+            }
+            set
+            {
+            }
+        }
+        public clsSuppliers ThisSuppliers { get; set; }
+    }
+}
